819a23f7bc4a03f1b0ccc92817a1f2d421492844
Calcifer <shanna.renee.morris@gmail.com>
2023-04-17 13:08:18 -0700

    deleted instructional comments and added the current time color functionality to the color blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,18 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  $('button').on('click', function() {
-    localStorage.setItem($(this).parent().attr('id'), $(this).siblings('textarea').val());
-
-  })
-
-
+$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
 
 $('[id^="hour-"]').each(function ( i ) {
   var item = localStorage.getItem($(this).attr('id'));
   $(this).children('textarea').text(item);
 })
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
-  // TODO: Add code to display the current date in the header of the page.
-});
- 
+$('[id^="hour-"]').each(function ( i ) {
+  if ($(this).attr('id') === dayjs().format('[hour-]h')) {
+    $(this).toggleClass('present');
+    $('.present ~ div').toggleClass('future');
+    $('.present').prevAll().toggleClass('past');
+  }
+})
+
+$('button').on('click', function () {
+    localStorage.setItem($(this).parent().attr('id'), $(this).siblings('textarea').val());
+  })
